1e2b962ff02ed49392498298705577815cd43e76
mirelly <mirelly.thayna@academico.ifpb.edu.br>
2026-09-02 09:53:38 -0300

    primeiro commit

---
+++ src/test/java/ifgram/IfgramApplicationTests.java
@@ -0,0 +1,13 @@
+package ifgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
