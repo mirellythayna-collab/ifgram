41cf2e7a9dee48fc6dafc6c35b60955b6f97bda4
mirelly <mirelly.thayna@academico.ifpb.edu.br>
2026-09-03 08:30:46 -0300

    primeiro controller

--- src/main/java/ifgram/controller/UserController.java
+++ src/main/java/ifgram/controller/UserController.java
@@ -1,4 +1,20 @@
 package ifgram.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("users")
+
+    public class UserController {
+
+        @GetMapping
+
+        public String getUser() {
+
+            return "get user was called";
+
+        }
 }
+
